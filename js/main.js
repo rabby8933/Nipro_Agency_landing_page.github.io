8fa5411567f586a7a695210eea10948ff7a4b3cf
Fazle Rabby Khan <84661560+rabby8933@users.noreply.github.com>
2022-08-16 13:47:19 +0600

    wow js added

--- js/main.js
+++ js/main.js
@@ -1,32 +1,33 @@
 // init Isotope
-var $grid = $('.project-lists').isotope({
-      // options
+var $grid = $(".project-lists").isotope({
+  // options
 });
 // filter items on button click
-$('.filter-button-group').on('click', 'button', function () {
-      var filterValue = $(this).attr('data-filter');
-      $grid.isotope({ filter: filterValue });
+$(".filter-button-group").on("click", "button", function () {
+   $(this).addClass('active').siblings().removeClass('active');
+   var filterValue = $(this).attr("data-filter");
+  $grid.isotope({ filter: filterValue });
 });
 
 //Owl Carousel 2 function call
-$('.owl-carousel').owlCarousel({
-      loop: true,
-      margin: 10,
-      autoplay: true,
-      autoplayTimeout: 3000,
-      nav: false,
-      dots: false,
-      responsiveClass: true,
-      responsive: {
-            0: {
-                  items: 1,
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  dots: true,
+  navText:["PREV", "NEXT"],
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 1,
+    },
+    1000: {
+      items: 1,
+    },
+  },
+});
 
-            },
-            600: {
-                  items: 1,
-            },
-            1000: {
-                  items: 1,
-            }
-      }
-})
+// Wow js function call
+new WOW().init();
